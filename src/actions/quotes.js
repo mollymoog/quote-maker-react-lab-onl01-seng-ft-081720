1839ec95cac8781a43344bb25de3027d103427e6
mollymoog <mmkover@gmail.com>
2021-01-11 21:48:26 -0500

    commit tree 014f36b38326db8d310bec7d96b4d82ec54a0497

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,29 @@
 // TODO: Create action creators as defined in tests
+
+export const addQuote = quote => {
+    return {
+        type: 'ADD_QUOTE',
+        quote: Object.assign({}, quote, { votes: 0})
+    }
+}
+  
+export const removeQuote = quoteID => {
+    return {
+        type: 'REMOVE_QUOTE',
+        quoteId: quoteID
+    }
+}
+
+export const upvoteQuote = quoteID => {
+    return {
+        type: 'UPVOTE_QUOTE',
+        quoteId: quoteID
+    }
+}
+
+export const downvoteQuote = quoteID => {
+    return {
+        type: 'DOWNVOTE_QUOTE',
+        quoteId: quoteID
+    }
+}
